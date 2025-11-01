f4eca8659f8e67d039ecc06aa8deabd863f8b7eb
cksckckcks <chan679999@naver.com>
2025-11-01 15:57:21 +0900

    feat: 랜덤으로 생성된 로또 번호 검증 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,9 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == LOTTO_NUMBER_SIZE) { "[ERROR] 로또 번호는 ${LOTTO_NUMBER_SIZE}개여야 합니다." }
+        require(numbers.distinct().size == LOTTO_NUMBER_SIZE) { "[ERROR] 로또 번호는 중복이 없어야 합니다." }
+        require(numbers.all{ it in LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER}) { "[ERROR] 로또 당첨번호는 ${LOTTO_MIN_NUMBER}에서 ${LOTTO_MAX_NUMBER}사이의 정수여야 합니다." }
     }
 
-    // TODO: 추가 기능 구현
 }
