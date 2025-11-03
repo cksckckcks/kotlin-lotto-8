a059e0e66dabfb256a1eef8425caab2196f63b52
cksckckcks <chan679999@naver.com>
2025-11-03 17:23:53 +0900

    test: InputValidator 테스트 코드 업데이트

--- src/test/kotlin/lotto/InputValidatorTest.kt
+++ src/test/kotlin/lotto/InputValidatorTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.assertThrows
 
 class InputValidatorTest {
 	@Test
-	fun `구매금액이 1000원 이상이다`() {
+	fun `구매금액이 정상이다`() {
 		val amount = 8000
 
 		assertDoesNotThrow {
@@ -29,6 +29,82 @@ class InputValidatorTest {
 
 		assertThrows<IllegalArgumentException> {
 			InputValidator.validatePurchaseAmount(amount)
+		}
+	}
+
+	@Test
+	fun `로또 당첨 번호가 정상이다`() {
+		val winningNumber = listOf(1, 2, 3, 4, 5, 6)
+
+		assertDoesNotThrow {
+			InputValidator.validateWinningNumbers(winningNumber)
+		}
+	}
+
+	@Test
+	fun `로또 당첨 번호 갯수가 6개가 아니면 예외가 발생한다`() {
+		val winningNumbers = listOf(1, 2, 3, 4, 5)
+
+		assertThrows<IllegalArgumentException> {
+			InputValidator.validateWinningNumbers(winningNumbers)
+		}
+	}
+
+	@Test
+	fun `로또 당첨 번호 중 1 미만의 수가 있으면 예외가 발생한다`() {
+		val winningNumbers = listOf(1, 2, 3, 4, 5, -1)
+
+		assertThrows<IllegalArgumentException> {
+			InputValidator.validateWinningNumbers(winningNumbers)
+		}
+	}
+
+	@Test
+	fun `로또 당첨 번호 중 45를 초과하는 수가 있으면 예외가 발생한다`() {
+		val winningNumbers = listOf(1, 2, 3, 4, 5, 46)
+
+		assertThrows<IllegalArgumentException> {
+			InputValidator.validateWinningNumbers(winningNumbers)
+		}
+	}
+
+	@Test
+	fun `보너스 번호가 정상이다`() {
+		val winningNumber = listOf(1, 2, 3, 4, 5, 6)
+		val bonusNumber = 7
+
+		assertDoesNotThrow {
+			InputValidator.validateLottoBonusNumber(winningNumber, bonusNumber)
+		}
+	}
+
+	@Test
+	fun `보너스 번호가 로또 당첨번호와 겹치면 예외가 발생한다`() {
+		val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+		val bonusNumber = 1
+
+		assertThrows<IllegalArgumentException> {
+			InputValidator.validateLottoBonusNumber(winningNumbers, bonusNumber)
+		}
+	}
+
+	@Test
+	fun `보너스 번호가 1 미만의 수가 있으면 예외가 발생한다`() {
+		val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+		val bonusNumber = 0
+
+		assertThrows<IllegalArgumentException> {
+			InputValidator.validateLottoBonusNumber(winningNumbers, bonusNumber)
+		}
+	}
+
+	@Test
+	fun `보너스 번호가 45를 초과하는 수가 있으면 예외가 발생한다`() {
+		val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+		val bonusNumber = 46
+
+		assertThrows<IllegalArgumentException> {
+			InputValidator.validateLottoBonusNumber(winningNumbers, bonusNumber)
 		}
 	}
 }
