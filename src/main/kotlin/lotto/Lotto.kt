44cad7caf24ca423bdfde8e5b7dec273a6fd82ad
cksckckcks <chan679999@naver.com>
2026-01-09 17:17:14 +0900

    fix: Lotto 클래스 검증 로직 중복 버그 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -7,7 +7,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
         require(numbers.toSet().size == numbers.size) { "[ERROR] 로또 번호는 중복되지 않아야 합니다." }
-        require(numbers.toSet().size == numbers.size) { "[ERROR] 로또 번호는 $LOTTO_MIN_NUMBER ~ $LOTTO_MAX_NUMBER 사이여야 합니다." }
+        require(numbers.all { it in LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER}) { "[ERROR] 로또 번호는 $LOTTO_MIN_NUMBER ~ $LOTTO_MAX_NUMBER 사이여야 합니다." }
     }
 
     fun getNumbers() = numbers
