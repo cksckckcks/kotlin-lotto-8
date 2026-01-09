49586de8275ed9faa83e97bf4aa248c8f0ce1fff
cksckckcks <chan679999@naver.com>
2026-01-09 17:35:01 +0900

    test: LottoResult 테스트 코드 작성

---
+++ src/test/kotlin/lotto/LottoResultTest.kt
@@ -0,0 +1,39 @@
+package lotto
+
+import lotto.model.LottoRank
+import lotto.model.LottoResult
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoResultTest {
+	@Test
+	fun `결과 카운트 테스트`() {
+		val amount = 4000
+		val ranks = listOf(LottoRank.FIRST, LottoRank.SECOND, LottoRank.NONE, LottoRank.NONE)
+
+		val lottoResult = LottoResult(amount, ranks)
+
+		val result = mapOf(
+			LottoRank.FIFTH to 0,
+			LottoRank.FOURTH to 0,
+			LottoRank.THIRD to 0,
+			LottoRank.SECOND to 1,
+			LottoRank.FIRST to 1
+		)
+
+		assertThat(lottoResult.getResult()).isEqualTo(result)
+	}
+
+	@Test
+	fun `수익률 테스트`() {
+		val amount = 8000
+		val ranks = listOf(LottoRank.FIFTH, LottoRank.NONE, LottoRank.NONE, LottoRank.NONE)
+
+		val lottoResult = LottoResult(amount, ranks)
+
+		val rateOfReturn = 62.5
+
+		assertThat(lottoResult.getWinningRateOfReturn()).isEqualTo(rateOfReturn)
+	}
+
+}
